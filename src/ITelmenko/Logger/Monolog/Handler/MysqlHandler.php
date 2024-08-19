4c093ac76f726c6740db7c858b2db57fed092e09
Alberto Sanchez <alberroteran@gmail.com>
2024-08-19 15:05:15 -0300

    Fixed handler

--- src/ITelmenko/Logger/Monolog/Handler/MysqlHandler.php
+++ src/ITelmenko/Logger/Monolog/Handler/MysqlHandler.php
@@ -4,14 +4,15 @@ namespace ITelmenko\Logger\Monolog\Handler;
 
 use Monolog\Handler\AbstractProcessingHandler;
 use ITelmenko\Logger\Laravel\Models\Log;
+use Monolog\LogRecord;
 
 class MysqlHandler extends AbstractProcessingHandler
 {
-    protected function write(array $record):void
+    protected function write(LogRecord $record):void
     {
         Log::create([
-            'instance'    => gethostname(),
-            'channel'     => $record['channel'],
+            'instance' => gethostname(),
+            'channel'  => $record['channel'],
             'message' => $record['message'],
             'level'   => $record['level_name'],
             'context' => $record['context']
